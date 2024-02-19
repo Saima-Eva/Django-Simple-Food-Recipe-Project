46946816869ccd2731e6736d3bba9f0d47a7caf1
Saima-Eva <saimayesmin99@gmail.com>
2024-02-19 11:33:19 +0600

    first commit

---
+++ myProject/myProject/urls.py
@@ -0,0 +1,34 @@
+
+from django.contrib import admin
+from django.urls import path , include
+from django.conf.urls.static import static
+from django.conf import settings
+
+import notifications.urls
+
+from .views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', signupPage, name='signupPage'),
+    path('logoutPage/', logoutPage, name='logoutPage'),
+    path('mySigninPage/', mySigninPage, name='mySigninPage'),
+    path('dashBoardPage/', dashBoardPage, name='dashBoardPage'),
+    path('forget_pass/', forget_pass, name='forget_pass'),
+    path('update_pass/', update_pass, name='update_pass'),
+    path('addRecipePage/', addRecipePage, name='addRecipePage'),
+    path('recipe_categories/', recipe_categories, name='recipe_categories'),
+    path('search_results/', search_results, name='search_results'),
+    path('viewRecipePage/', viewRecipePage, name='viewRecipePage'),
+    path('view_favorites/', view_favorites, name='view_favorites'),
+    path('add_to_favorites/<int:recipe_id>/', add_to_favorites, name='add_to_favorites'),
+    path('AddRecipeCategoryPage/', AddRecipeCategoryPage, name='AddRecipeCategoryPage'),
+    path('activate/<uid64>/<token>', activate,name='activate'),
+    path('edit_recipe/<str:myid>', edit_recipe,name='edit_recipe'),
+    path('delete_recipe/<str:myid>', delete_recipe,name='delete_recipe'),
+    
+#notifications
+    path('notifications/', include(notifications.urls,namespace='notifications')),
+    path('notificationPage/', notificationPage, name='notificationPage'),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
